69db862b6abe1ac6c13d0543671f8536f46e3080
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-12-15 16:20:23 -0600

    chore: debug hostname

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,7 @@ app.post('/', async (req: any, res: any) => {
     // validate it is valid hostname for this key
 
     const hostname = new URL(headers.Origin || headers.origin).hostname
+    console.log({ hostname })
     if (!user?.domain_names?.split(',').includes(hostname)) return res.sendStatus(401).end()
 
     // https://hasura.io/docs/latest/graphql/core/auth/authorization/index.html
